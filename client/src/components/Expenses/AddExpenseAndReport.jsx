4fa5a4c7d4d21de4cce2e2ab5584a502758cfbf7
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-11-25 14:53:15 +0500

    Responsiveness Done

---
+++ client/src/components/Expenses/AddExpenseAndReport.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaFileInvoiceDollar, FaChartPie } from 'react-icons/fa';
+
+const AddExpenseAndReport = () => {
+   return (
+      <>
+         <div className="p-6 bg-gray-900 rounded-md text-white w-full -mt-6">
+            <h2 className="text-xl font-semibold mb-4">Quick Access</h2>
+            <div className="flex justify-center w-full">
+               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 -mt-5">
+                  {/* New Expense Button */}
+                  <div className="flex items-center p-4 bg-gray-800 rounded-lg shadow-md hover:bg-gray-700 transition">
+                     <div className="w-10 h-10 flex items-center justify-center text-white rounded-full bg-pink-600 mr-4">
+                        <FaFileInvoiceDollar />
+                     </div>
+                     <span className="text-sm font-medium">+ New expense</span>
+                  </div>
+
+                  {/* Create Report Button */}
+                  <div className="flex items-center p-4 bg-gray-800 rounded-lg shadow-md hover:bg-gray-700 transition">
+                     <div className="w-10 h-10 flex items-center justify-center text-white rounded-full bg-teal-600 mr-4">
+                        <FaChartPie />
+                     </div>
+                     <span className="text-sm font-medium">+ Create report</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default AddExpenseAndReport;
--- client/src/components/MainExpenses.jsx
+++ client/src/components/MainExpenses.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import React from 'react';
+import ExpensesRecord from './Expenses/ExpensesRecord';
+import MonthlyReportBarGraph from './Dashboard/MonthlyReportBarGraph';
+import WeeklyReportBarGraph from './Dashboard/WeeklyReportBarGraph';
+import TrackFinance from './Dashboard/TrackFinance';
+import AddExpenseAndReport from './Expenses/AddExpenseAndReport';
+import Categories from './Dashboard/Catagories';
 
 function MainExpenses() {
    return (
-      <>
-         <h1 className='text-center justify-center bg-white'>Expenses Section</h1>
-      </>
+      <div className="flex flex-col md:flex-row gap-6 mt-10 ">
+         {/* Left Column */}
+         <div className="flex-grow ">
+            <ExpensesRecord />
+            <AddExpenseAndReport />
+         </div>
+
+         {/* Right Column */}
+         <div className="flex-1 space-y-6 overflow-hidden mr-4">
+            <div>
+               <TrackFinance />
+            </div>
+            <div>
+               {/* <MonthlyReportBarGraph /> */}
+               <Categories />
+            </div>
+         </div>
+      </div>
    );
 }
 
